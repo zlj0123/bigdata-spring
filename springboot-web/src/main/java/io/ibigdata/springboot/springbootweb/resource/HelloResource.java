eb52dc6000b4aa20d87384564edd1cad430504fa
zlj0123 <zlj0123@gmail.com>
2021-06-08 14:16:53 +0800

    commit
    commit

---
+++ springboot-web/src/main/java/io/ibigdata/springboot/springbootweb/resource/HelloResource.java
@@ -0,0 +1,13 @@
+package io.ibigdata.springboot.springbootweb.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloResource {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+}
